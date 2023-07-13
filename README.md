2149817241bac4cf8fca5de85ef28337c86766c7
Amey Varhade <ameyvarhade@gmail.com>
2023-07-13 14:32:35 +0530

    update model and data

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Weight Decomposition for PINNs
 
 Exploring Matrix Decomposition to remove weight spaces fluctuations and dissimilarities in context of PDEs
+
+
+## Experiment Setup and Structure
+
+There are 100 boundary points along each side of the domain (each edge) of the rectangle. We have 2500 colocation points sampled using the latin hypercube sampling in the cartesian rectangular domain
--- pinn_model.py
+++ pinn_model.py
@@ -6,9 +6,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import torch.optim.lr_scheduler as lr_scheduler
 import argparse
 import h5py
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+random_seed = 1
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed(random_seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(random_seed)
 
 
 parser = argparse.ArgumentParser(description ='Reynolds Number')
@@ -17,18 +25,18 @@ args = parser.parse_args()
 Re = args.Re
 nu = 1/Re
 
-f1 = open('pinns\\training_details\Re_{}.txt'.format(args.Re), 'a+')
-f1.write('Reynolds Number: {}\n'.format(args.Re))
+f1 = open('pinns/pretrained/training_details/Re_{}.txt'.format(Re), 'a+')
+f1.write('Reynolds Number: {}\n'.format(Re))
 
 
-f = h5py.File('data\\boundary_points.h5', 'r')
+f = h5py.File('data/boundary_points.h5', 'r')
 X_bc = f['X_bc']
 Y_bc = f['Y_bc']
 X_bc = np.asarray(X_bc)
 Y_bc = np.asarray(Y_bc)
 f.close()
 
-f = h5py.File('data\Analytical_Solutions\\boundary\Re_{}.h5'.format(args.Re), 'r')
+f = h5py.File('data/Analytical_Solutions/boundary/Re_{}.h5'.format(Re), 'r')
 S_bc = f['S_bc']
 S_bc = np.asarray(S_bc)
 f.close()
@@ -39,21 +47,21 @@ S_bc = Variable(torch.from_numpy(S_bc).float(), requires_grad=False).to(device)
 
 
 
-f = h5py.File('data\colocation_points.h5', 'r')
+f = h5py.File('data/colocation_points.h5', 'r')
 X_col = f['X_col']
 Y_col = f['Y_col']
 X_col = np.asarray(X_col)
 Y_col = np.asarray(Y_col)
 f.close()
 
-f = h5py.File('data\Analytical_Solutions\colocation\Re_{}.h5'.format(args.Re), 'r')
+f = h5py.File('data/Analytical_Solutions/colocation/Re_{}.h5'.format(Re), 'r')
 S_col = f['S_col']
 S_col = np.asarray(S_col)
 f.close()
 
 X_col = Variable(torch.from_numpy(X_col).float(), requires_grad=True).to(device)
 Y_col = Variable(torch.from_numpy(Y_col).float(), requires_grad=True).to(device)
-S_col = Variable(torch.from_numpy(S_col).float(), requires_grad=False).to(device)
+S_col = Variable(torch.from_numpy(S_col).float(), requires_grad=True).to(device)
 S = torch.zeros_like(X_col).to(device)
 
 
@@ -65,6 +73,7 @@ class PINN(nn.Module):
         self.layer3 = nn.Linear(20,20)
         self.layer4 = nn.Linear(20,20)
         self.layer5 = nn.Linear(20,20)
+        self.layer6 = nn.Linear(20,20)
         self.output_layer = nn.Linear(20,3)
 
     def forward(self, x, y): 
@@ -74,13 +83,15 @@ class PINN(nn.Module):
         layer3_out = torch.tanh(self.layer3(layer2_out))
         layer4_out = torch.tanh(self.layer4(layer3_out))
         layer5_out = torch.tanh(self.layer5(layer4_out))
-        output = self.output_layer(layer5_out) 
+        layer6_out = torch.tanh(self.layer6(layer5_out))
+        output = self.output_layer(layer6_out) 
         return output
     
 pinn = PINN()
 pinn = pinn.to(device)
 mse_cost_function = nn.MSELoss() 
-optimizer = torch.optim.Adam(pinn.parameters())
+optimizer = torch.optim.Adam(pinn.parameters(), lr=1e-2)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=8000, gamma=0.1, last_epoch=-1, verbose=False)
 
 def residual(x, y, pinn):
     s = pinn.forward(x, y)
@@ -108,7 +119,7 @@ def residual(x, y, pinn):
     return f
 
 
-iterations = 10000
+iterations = 50000
 for epoch in range(iterations):
     optimizer.zero_grad()
     
@@ -124,8 +135,9 @@ for epoch in range(iterations):
     optimizer.step()
 
     with torch.autograd.no_grad():
-        if epoch%500 == 0:
-            f1.write('Iter %d, Loss: %.4e, Loss_u: %.4e, Loss_f: %.4e \n' % (epoch, Loss, MSE_U, MSE_F))
+        if epoch%1000 == 0:
+            current_lr = optimizer.param_groups[0]['lr']
+            f1.write('Epoch %d, LR: %.4e, Loss: %.4e, Data Loss: %.4e, Physics Loss: %.4e' % (epoch, current_lr, Loss, MSE_U, MSE_F))
 
 # Relative L2 Error
 S_pinn = pinn(X_col, Y_col)
@@ -141,4 +153,6 @@ for key in params.keys():
     for val in flattened_tensor:
         flattened_weights.append(val)
 weights_array = np.array(flattened_weights)
-np.savetxt('pinns/weights/weights_'+str(Re)+'.txt', weights_array)
+np.savetxt('pinns/pretrained/weights/weights_'+str(Re)+'.txt', weights_array)
+
+torch.save(pinn.state_dict(), 'pinns/pretrained/weights/model_'+str(Re)+'.pt')
